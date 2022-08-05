4f451d3acaaf35ef50473399fb2ccf107603e8d1
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-05 23:01:41 +0300

    практика

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,15 +1,25 @@
 
 const input = document.querySelector('#validation-input')
 
-input.addEventListener('blur',() => {
-  
-    if (input.value.length == input.dataset.length) {
+input.addEventListener('blur', onInputBlur)
+
+
+function onInputBlur() {
+       if (Number(input.value.length) === Number(input.dataset.length)) {
         input.classList.remove('invalid')
-       return input.classList.add('valid')
-    } else 
-        input.classList.remove('valid')
- input.classList.add('invalid')
+           input.classList.add('valid')
+           console.log('это иф');
+       } else {
+            input.classList.remove('valid')
+    input.classList.add('invalid')
+    console.log(Number(input.value.length))
+    console.log(input.dataset.length);   
+    }
+    
+
+  }
+ 
   
-})
+
 
  
