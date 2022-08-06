f5c8a2f2e0344c23db18449fb463b7c1dc7d6220
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-08-07 00:18:40 +0300

    перестановка

--- js/task-08.js
+++ js/task-08.js
@@ -3,14 +3,15 @@ const form = document.querySelector('.login-form')
 
 form.addEventListener('submit', (evt) => {
     evt.preventDefault()
+    
     if (evt.currentTarget.elements.email.value === '' || 
         evt.currentTarget.elements.password.value === '') {
         alert('Все поля формы должны быть заполнены')
     }
-    const userObj = {}
-    userObj.email = evt.currentTarget.elements.email.value
-    userObj.password =  evt.currentTarget.elements.password.value 
-
+    const userObj = {
+        email: evt.currentTarget.elements.email.value,
+       password: evt.currentTarget.elements.password.value 
+    }
     console.log(userObj);
     evt.currentTarget.reset()
 })
